3aa802351f1e4e213cf620d7d343d94a166b49f7
Mike Conway <Michael.c.conway@gmail.com>
2023-04-10 08:41:45 -0400

    add local integration test script

--- ga4gh-console/src/main/java/org/irods/jargon/ga4gh/dos/console/utils/Ga4ghConsoleVersion.java
+++ ga4gh-console/src/main/java/org/irods/jargon/ga4gh/dos/console/utils/Ga4ghConsoleVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.ga4gh.dos.console.utils;
 public final class Ga4ghConsoleVersion {
  public static String VERSION="0.0.2-SNAPSHOT";
- public static String BUILD_TIME="2022-07-22T15:24:30Z";
+ public static String BUILD_TIME="2023-04-10T12:30:17Z";
 }
